2cce9e9c4d2536f618dbd75e210d3f4e4c5eff5c
Marcela <arqmrcl@gmail.com>
2022-11-21 15:47:31 -0300

    09, criando classe monster

---
+++ src/Monster.ts
@@ -0,0 +1,37 @@
+import Fighter, { SimpleFighter } from './Fighter';
+
+class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor(lifePoints = 85, strength = 63) {
+    this._lifePoints = lifePoints;
+    this._strength = strength;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  // metodo implementado usando de referencia a branch do daniel rubens
+  receiveDamage(attackPoints: number): number {
+    const damage = attackPoints - this._lifePoints;
+    if (damage > 0) {
+      this._lifePoints -= damage;
+      if (this._lifePoints <= 0) {
+        this._lifePoints -= 1;
+      }
+    }
+    return this._lifePoints;
+  }
+
+  attack(enemy: Fighter | SimpleFighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+}
+
+export default Monster;
